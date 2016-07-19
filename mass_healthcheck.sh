c39e21bd670536638cca0c81cd4fc6b16e396fc5
Johanan Lai <lai.johanan@gmail.com>
2016-07-18 17:25:38 -0700

    Commit mass_healthcheck.sh

--- mass_healthcheck.sh
+++ mass_healthcheck.sh
@@ -2,7 +2,6 @@
 
 for image in `ls ./resources`; do
     for image_name in `docker search -s 50 $image | awk '{print $1}' | tail -n+2`; do
-        echo $image_name
         ./healthcheck.sh $image $image_name
     done
 done
